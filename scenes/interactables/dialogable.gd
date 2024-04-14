0507765fec5d744bea6f1f74768a5f9148550c47
StupidHobbit <3mir33@gmail.com>
2024-04-14 15:36:49 +0300

    Informer room

--- scenes/interactables/dialogable.gd
+++ scenes/interactables/dialogable.gd
@@ -3,6 +3,7 @@ extends Interactable
 @export var title: String
 @export var dialogue: DialogueResource
 @export var dialogue_title: String
+@export var prefix: String = 'Talk to '
 
 const BALLOON = preload("res://scenes/dialogue/balloon.tscn")
 
@@ -10,7 +11,7 @@ func _ready():
 	pass
 
 func get_label() -> String:
-	return "Talk to \"%s\"" % title
+	return prefix + title
 
 func on_interact(character: Character):
 	var balloon: Node = BALLOON.instantiate()
