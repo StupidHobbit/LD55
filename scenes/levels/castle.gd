2a1b2d9257db5bf0e6c9b90bc93abf297d056110
SayuriNyooko <sayuriauthor@gmail.com>
2024-04-16 02:22:54 +0300

    fix paimon

--- scenes/levels/castle.gd
+++ scenes/levels/castle.gd
@@ -4,4 +4,8 @@ func _ready():
 	#State.add_item('cats_eye')
 	#State.add_item('mandrake')
 	#State.add_item('informer_key')
+	#State.add_item('informer_key')
+	#State.add_item('chains')
+	#State.add_item('crest_amulet')
+	#State.add_item('blood')
 	pass
