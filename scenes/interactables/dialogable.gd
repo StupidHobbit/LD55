9fda2a123d85c1ec9967aa80454ca4b656cab0bb
StupidHobbit <3mir33@gmail.com>
2024-04-15 21:59:13 +0300

    added more sounds

--- scenes/interactables/dialogable.gd
+++ scenes/interactables/dialogable.gd
@@ -6,12 +6,21 @@ class_name Dialogable
 @export var dialogue: DialogueResource
 @export var dialogue_title: String
 @export var prefix: String = 'Talk to '
+@export var audio: AudioStream
+
+var player: AudioStreamPlayer
 
 func _ready():
-	pass
+	if audio != null:
+		var p = AudioStreamPlayer.new()
+		p.stream = audio
+		add_child(p)
+		player = p
 
 func get_label() -> String:
 	return prefix + title
 
 func on_interact(character: Character):
 	Utils.start_dialogue(dialogue, dialogue_title)
+	if player != null:
+		player.play()
